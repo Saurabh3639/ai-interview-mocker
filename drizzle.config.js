1d6a11c02cc3d14ba32e7c1dc496f9b154fa9ff7
Saurabh Phawade <saurvphawade09@gmail.com>
2024-06-22 18:01:26 +0530

    Updated dbCredentials url in drizzle.config.js to use NEXT_PUBLIC_DRIZZLE_DB_URL environment variable

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
   schema: "./utils/schema.js",
   dialect: "postgresql",
   dbCredentials: {
-    url: "postgresql://ai-interview-mocker_owner:rkM3O4tTnCaB@ep-lively-dream-a1xscudz.ap-southeast-1.aws.neon.tech/ai-interview-mocker?sslmode=require",
+    url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL,
   },
 };
